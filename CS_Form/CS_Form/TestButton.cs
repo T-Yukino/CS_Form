6b47a6a5737b52b84ca6ac6115cec4c63c3e29e0
tamoyou1230 <tamoyou1230@gmail.com>
2024-10-30 12:20:19 +0900

    TestButton.csの作成

---
+++ CS_Form/CS_Form/TestButton.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Windows.Forms;
+
+namespace CS_Form
+{
+    class TestButton : Button
+    {
+        /// <summary>
+        /// コンストラクタ
+        /// クラスを生成したときに呼び出される
+        /// </summary>
+        public TestButton()
+        {
+            //ClickイベントにOnClick関数を登録
+            Click += onclick;
+        }
+
+        public void onclick(object sender,EventArgs s)
+        {
+            MessageBox.Show("( *´艸｀)");
+        }
+
+    }
+}
