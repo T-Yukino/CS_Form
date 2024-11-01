056b5fc0510be276c531b73f48905b2ef3a7c59f
tamoyou1230 <tamoyou1230@gmail.com>
2024-11-01 14:24:26 +0900

    ボタン増やす

--- CS_Form/CS_Form/TestButton.cs
+++ CS_Form/CS_Form/TestButton.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,14 +11,23 @@ namespace CS_Form
 {
     class TestButton : Button
     {
+        private ColorDialog colorDialog1;
+
         /// <summary>
         /// コンストラクタ
         /// クラスを生成したときに呼び出される
         /// </summary>
-        public TestButton()
+        public TestButton(int x, int y)
         {
             //ClickイベントにOnClick関数を登録
+            //ボタンをクリックした時に登録
             Click += onclick;
+
+            //ボタン内に文字を表示
+            Text = "ボタン";
+
+            Location = new Point(x,y);
+
         }
 
         public void onclick(object sender,EventArgs s)
